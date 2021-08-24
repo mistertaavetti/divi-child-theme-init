0ab2d91f0d92324bf115f363c2b5cbaf35e81d7b
Mister Taavetti <36891139+mistertaavetti@users.noreply.github.com>
2021-08-24 05:21:32 +0100

    Slick JS/CSS for Ultimate Divi Modules Carousel

--- functions.php
+++ functions.php
@@ -4,4 +4,12 @@
 	    wp_enqueue_style( 'divi', get_template_directory_uri() . '/style.css' );
       wp_enqueue_script( 'divi', plugin_dir_url( __FILE__ ) . 'js/scripts.js', array( 'jquery', 'divi-custom-script' ), '0.1.2', true );
 	}
+
+	add_action( 'wp_enqueue_scripts', 'dlspc_enqueue_styles' );
+	function dlspc_enqueue_styles() {
+	    wp_enqueue_style( 'dlspc-style', get_template_directory_uri() . '/style.css' );
+		wp_enqueue_style( 'dlspc-slickmincss','https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css');
+		wp_enqueue_style( 'dlspc-slick-thememincss','https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css' );
+		wp_enqueue_script( 'dlspc-slickminjs', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js',array( 'jquery' ) ,'1.0',true );
+	}
 ?>
